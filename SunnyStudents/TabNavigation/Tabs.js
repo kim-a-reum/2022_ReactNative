4594697521a889dfd8db59d9ac1e8c2f5c4061da
kim-a-reum <96907807+kim-a-reum@users.noreply.github.com>
2022-09-18 23:01:29 +0900

    useColorScheme

--- SunnyStudents/TabNavigation/Tabs.js
+++ SunnyStudents/TabNavigation/Tabs.js
@@ -4,53 +4,61 @@ import Search from "../screens/Search";
 import Student from "../screens/Student";
 import { FontAwesome5 } from "@expo/vector-icons";
 import { Ionicons } from "@expo/vector-icons";
+import { useColorScheme } from "react-native";
 
 const Tab = createBottomTabNavigator();
 
-const Tabs = () => (
-  <Tab.Navigator
-    initialRouteName="Search"
-    screenOptions={{
-      tabBarLabelStyle: {
-        color: "#696969",
-        fontSize: 12.5,
-      },
-      tabBarStyle: { height: 85 },
-      headerShown: false,
-    }}
-  >
-    <Tab.Screen
-      name="강의 목록"
-      component={Class}
-      options={{
-        tabBarIcon: () => (
-          <FontAwesome5 name="chalkboard-teacher" size={28} color="#FFE100" />
-        ),
-      }}
-    />
-    <Tab.Screen
-      name="학생 목록"
-      component={Student}
-      options={{
-        tabBarIcon: () => <Ionicons name="people" size={33} color="#FFE100" />,
+const Tabs = () => {
+  const colorScheme = useColorScheme();
+  console.log(colorScheme);
 
-        tabBarBadge: "5",
-        tabBarBadgeStyle: {
-          width: 20,
-          height: 15,
-          paddingBottom: 18,
-          marginLeft: 4,
+  return (
+    <Tab.Navigator
+      initialRouteName="Search"
+      screenOptions={{
+        tabBarLabelStyle: {
+          color: "#696969",
+          fontSize: 12.5,
         },
+        tabBarStyle: { height: 85 },
+        headerShown: false,
       }}
-    />
-    <Tab.Screen
-      name="통합 검색"
-      component={Search}
-      options={{
-        tabBarIcon: () => <Ionicons name="search" size={24} color="black" />,
-      }}
-    />
-  </Tab.Navigator>
-);
+    >
+      <Tab.Screen
+        name="강의 목록"
+        component={Class}
+        options={{
+          tabBarIcon: () => (
+            <FontAwesome5 name="chalkboard-teacher" size={28} color="#FFE100" />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="학생 목록"
+        component={Student}
+        options={{
+          tabBarIcon: () => (
+            <Ionicons name="people" size={33} color="#FFE100" />
+          ),
+
+          tabBarBadge: "5",
+          tabBarBadgeStyle: {
+            width: 20,
+            height: 15,
+            paddingBottom: 18,
+            marginLeft: 4,
+          },
+        }}
+      />
+      <Tab.Screen
+        name="통합 검색"
+        component={Search}
+        options={{
+          tabBarIcon: () => <Ionicons name="search" size={24} color="black" />,
+        }}
+      />
+    </Tab.Navigator>
+  );
+};
 
 export default Tabs;
