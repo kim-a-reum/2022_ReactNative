f167736c38fd947d7e183c5fdf93b6ac0b614b06
kim-a-reum <96907807+kim-a-reum@users.noreply.github.com>
2022-09-28 12:23:06 +0900

    styled-components

--- SunnyStudents/screens/Class.tsx
+++ SunnyStudents/screens/Class.tsx
@@ -1,12 +1,11 @@
 import React from "react";
 import { Text } from "react-native";
 import styled from "styled-components/native";
-
 const Wrapper = styled.View`
   flex: 1;
   justify-content: center;
   align-items: center;
-  background-color: ${(props) => props.theme.mainBgColor};
+  background-color: ${(props: any) => props.theme.mainBgColor};
 `;
 
 const Class = () => {
