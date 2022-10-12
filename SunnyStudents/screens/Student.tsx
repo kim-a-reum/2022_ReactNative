d9188a9e0df8379eff4afcacdfae27575da61668
kim-a-reum <96907807+kim-a-reum@users.noreply.github.com>
2022-10-13 08:51:21 +0900

    Students List

--- SunnyStudents/screens/Student.tsx
+++ SunnyStudents/screens/Student.tsx
@@ -1,13 +1,40 @@
 import React, { useEffect, useState } from "react";
-import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  Image,
+  ScrollView,
+} from "react-native";
+import { makeStudentImg } from "../utils";
+import styled from "styled-components/native";
+import Swiper from "react-native-web-swiper";
 
+const StudentImg = styled.Image`
+  width: 200px;
+  height: 200px;
+`;
+const Scroll = styled.ScrollView`
+  width: 100%;
+  height: 200px;
+`;
+const ScrollWrapper = styled.View`
+  width: 400px;
+  height: 200px;
+`;
+const StudentDetail = styled.View`
+  width: 100px;
+  height: 200px;
+  background-color: pink;
+`;
 const API_KEY = "609d02fc0e4f67ea6646dc7de401b14e";
 
-const Student = ({ navigation }) => {
+const Student = () => {
   const [students, setStudents] = useState([]);
   const studentData = async () => {
     const arr = [];
-    for (let i = 1; i < 3; i++) {
+    for (let i = 1; i < 4; i++) {
       const { biography, ...json } = await (
         await fetch(
           `https://api.themoviedb.org/3/person/${i}?api_key=${API_KEY}&language=en-US`
@@ -25,21 +52,18 @@ const Student = ({ navigation }) => {
   }, []);
 
   return (
-    <TouchableOpacity
-      onPress={() => navigation.navigate("Stack", { screen: "aaa" })}
-      style={styles.container}
-    >
-      <View>
+    <View>
+      <Scroll horizontal>
         {students.map((el) => (
-          <Text key={el.imdb_id}>{el.birthday}</Text>
+          <ScrollWrapper key={el.imdb_id}>
+            <StudentImg source={{ uri: makeStudentImg(el.profile_path) }} />
+
+            <StudentDetail></StudentDetail>
+          </ScrollWrapper>
         ))}
-      </View>
-    </TouchableOpacity>
+      </Scroll>
+    </View>
   );
 };
-
-const styles = StyleSheet.create({
-  container: { flex: 1, justifyContent: "center", alignItems: "center" },
-});
 
 export default Student;
--- SunnyStudents/utils.ts
+++ SunnyStudents/utils.ts
@@ -1,3 +1,4 @@
+// 학생 이미지 url 제공함수
 export const makeStudentImg = (img: string, width: string = "w200") => {
-  return `https://image.tmdb.org/t/p${width}${img}`;
+  return `https://image.tmdb.org/t/p/${width}${img}`;
 };
