d26f35611a42ad44f31ab5b8e668bde779722bbe
kim-a-reum <96907807+kim-a-reum@users.noreply.github.com>
2022-10-19 18:04:17 +0900

    student Detail

--- SunnyStudents/screens/Student.tsx
+++ SunnyStudents/screens/Student.tsx
@@ -22,11 +22,17 @@ const Scroll = styled.ScrollView`
 const ScrollWrapper = styled.View`
   width: 400px;
   height: 200px;
+  display: flex;
+  flex-direction: row;
 `;
 const StudentDetail = styled.View`
-  width: 100px;
+  width: 200px;
   height: 200px;
-  background-color: pink;
+  background-color: GRAY;
+  padding: 20px;
+`;
+const DetailContents = styled.Text`
+  size: 13px;
 `;
 const API_KEY = "609d02fc0e4f67ea6646dc7de401b14e";
 
@@ -34,7 +40,7 @@ const Student = () => {
   const [students, setStudents] = useState([]);
   const studentData = async () => {
     const arr = [];
-    for (let i = 1; i < 4; i++) {
+    for (let i = 1; i < 10; i++) {
       const { biography, ...json } = await (
         await fetch(
           `https://api.themoviedb.org/3/person/${i}?api_key=${API_KEY}&language=en-US`
@@ -57,8 +63,9 @@ const Student = () => {
         {students.map((el) => (
           <ScrollWrapper key={el.imdb_id}>
             <StudentImg source={{ uri: makeStudentImg(el.profile_path) }} />
-
-            <StudentDetail></StudentDetail>
+            <StudentDetail>
+              <DetailContents> 이름 : {el.name}</DetailContents>
+            </StudentDetail>
           </ScrollWrapper>
         ))}
       </Scroll>
